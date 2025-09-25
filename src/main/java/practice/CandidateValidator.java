5000dd750377342c37b9fd10326c69a510385812
nowakjd <JedrzejNowak@protonmail.com>
2025-09-25 06:38:17 +0200

    Praticing streams

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,30 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.Arrays;
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    public static final String SEPARATOR_FOR_PERIODS = ",";
+    public static final String SEPARATOR_FOR_DATES = "-";
+    public static final int ELIGIBLE_AGE = 35;
+    public static final String ELIGIBLE_NATIONALITY = "Ukrainian";
+    public static final int ELIGIBLE_PERIOD = 10;
+
+    @Override
+    public boolean test(Candidate candidate) {
+        String[] periods = candidate.getPeriodsInUkr().split(SEPARATOR_FOR_PERIODS);
+        int totalPeriod = Arrays.stream(periods)
+                .mapToInt(period -> {
+                    String[] years = period.split(SEPARATOR_FOR_DATES);
+                    return Integer.parseInt(years[1])
+                            - Integer.parseInt(years[0]);
+                })
+                .sum();
+
+        return candidate.getAge() >= ELIGIBLE_AGE
+                && candidate.isAllowedToVote()
+                && candidate.getNationality().equals(ELIGIBLE_NATIONALITY)
+                && totalPeriod >= ELIGIBLE_PERIOD;
+    }
 }
